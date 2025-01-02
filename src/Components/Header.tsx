c24cd41358b266092ca62a463c16b8f070e9ee19
Yuri Santos <yuri.rps@gmail.com>
2025-01-02 17:25:40 -0300

    feat: sidenav

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,10 +4,13 @@ import Meses from './Meses'
 
 const Header = () => {
 
+    const [title, setTitle] = React.useState('Resumo')
+
     return (
         <header className='mb'>
-            <div className='mb'>
+            <div className= 'daterange mb'>
                 <DateRange />
+                <h1 className='box bg-3'>{title}</h1>
             </div>
             <Meses />
         </header>
--- src/Components/Sidenav.tsx
+++ src/Components/Sidenav.tsx
@@ -1,8 +1,62 @@
 import React from 'react'
+import fintech from '../assets/fintech.svg'
+import resumo from '../assets/icons/resumo.svg'
+import vendas from '../assets/icons/vendas.svg'
+import webhooks from '../assets/icons/webhooks.svg'
+import configuracoes from '../assets/icons/configuracoes.svg'
+import contato from '../assets/icons/contato.svg'
+import sair from '../assets/icons/sair.svg'
+import FintechSVG from '../assets/FintechSVG'
+
 
 const Sidenav = () => {
   return (
-    <div>Sidenav</div>
+    <nav className='sidenav box bg-3 '>
+      <FintechSVG title='Fintech logo' />
+      <ul>
+        <li>
+          <span>
+            <img src={resumo} alt="" />
+          </span>
+          <a href="">Resumo</a>
+        </li>
+
+        <li>
+          <span>
+            <img src={vendas} alt="" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+
+        <li>
+          <span>
+            <img src={webhooks} alt="" />
+          </span>
+          <a href="">Webhooks</a>
+        </li>
+        
+        <li>
+          <span>
+            <img src={configuracoes} alt="" />
+          </span>
+          <a href="">Configurações</a>
+        </li>
+        
+        <li>
+          <span>
+            <img src={contato} alt="" />
+          </span>
+          <a href="">Contato</a>
+        </li>
+        
+        <li>
+          <span>
+            <img src={sair} alt="" />
+          </span>
+          <a href="">Sair</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
