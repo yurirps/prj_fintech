0e062274ee8974f5f2350511789b2872880c704f
Yuri Santos <yuri.rps@gmail.com>
2025-01-02 14:20:07 -0300

    feat: resumo de vendas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Header from './Components/Header'
 import Sidenav from './Components/Sidenav'
 import { DataContextProvider } from './Context/DataContext'
 import Resumo from './Pages/Resumo'
+import Vendas from './Pages/Vendas'
 import './Style.css'
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
         <Sidenav />
         <main>
           <Header />
+          <Resumo />
+          <Vendas />
         </main>
-        <Resumo />
       </div>
     </DataContextProvider>
   )
--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -2,10 +2,43 @@ import React from 'react'
 import { useData } from '../Context/DataContext'
 
 const Resumo = () => {
- const { data } = useData()
-     console.log(data)
+  const { data } = useData()
+  console.log('resumo aqui', data)
+  if (data === null) return null
   return (
-    <div>Resumo</div>
+    <section>
+      <div className='resumo flex mb'>
+        <div className='box'>
+          <h2>Total de Vendas</h2>
+          <span>
+            {data.filter((i) => i.status !== 'falha')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+
+        <div className='box'>
+          <h2>Recebido</h2>
+          <span>
+            {data.filter((i) => i.status === 'pago')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+
+        <div className='box'>
+          <h2>Processando</h2>
+          <span>
+            {data.filter((i) => i.status === 'processando')
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}
+          </span>
+        </div>
+      </div>
+      <div className='box mb'>
+              graficos
+      </div>
+    </section>
   )
 }
 
